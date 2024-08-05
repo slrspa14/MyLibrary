f95d08c0361f817428b386b6f8e8e90790e4c7b7
bh.cho <bh.cho@gmail.com>
2024-08-05 19:39:35 +0900

    Attribute

--- ClassToIni/ClassToIni.cs
+++ ClassToIni/ClassToIni.cs
@@ -1,32 +1,73 @@
 ﻿using System.Runtime.InteropServices;
 using System.Text;
+using System;
+using static Program;
+using System.IO;
+using java.util;
 
 class ClassToIni
 {
+    private static string mFilePath = "d";
+    private static string mFileName = "test.ini";
+    private static string mFolderPath = "D:\\study_project\\Library\\MyLibrary\\ClassToIni";
+    private DirectoryInfo mDirectory = new DirectoryInfo(mFolderPath);
+
+
     [DllImport("kernel32")]
-    private static extern long WritePrivateProfileString(string section, string key, string value, string filePath);
+    private static extern long WritePrivateProfileString(int Test1, string Test2, Enum Test3, bool Test4, float Test5, double Test6);
     [DllImport("kernel32")]
     private static extern long GetPrivateProfileString(string section, string key, string def, StringBuilder reVal, int size, string filePath);
+    public sealed class SectionName : Attribute
+    {
+        public string Name { get; }
+        public SectionName(string name)
+        {
+            Name = name;
+        }
+    }
+
+    public sealed class UseINI : Attribute
+    {
+        public bool status;
+        public UseINI(bool status)
+        {
+            this.status = status;
+        }
+    }
+    [SectionName("Base")]
+    public int Test1 { get; set; } = 0; //기본값
+    public string Test2 { get; set; } = ""; //기본값
+    public ETest Test3 { get; set; } = ETest.A; //기본값
+    public bool Test4 { get; set; } = false; //기본값
+    public float Test5 { get; set; } = 0.0f; //기본값
+    [SectionName("Hi")]
+    public double Test6 { get; set; } = 0.0; //기본값
     public ClassToIni(FileInfo fileName)
     {
 
     }
-    public void LoadINI()
+    public string LoadINI()
     {
-
+        StringBuilder result = new StringBuilder();
+        //그냥 경로 가서 ini 파일 읽어오고 보내서 출력확인하고
+        return result.ToString();
     }
     public void SaveINI() 
     {
-
+        //설정값 저장
     }
     public void CreateINI()
     {
+        if(!mDirectory.Exists)
+        {
+            WritePrivateProfileString(Test1, Test2, Test3, Test4, Test5, Test6);
+        }
 
     }
     
 
-    public static void Main(string[] args)
-    {
-        WritePrivateProfileString("test123", "xValue", "6", "D:\\study_project\\Library\\MyLibrary\\ClassToIni/test.ini");
-    }
+    //public static void Main(string[] args)
+    //{
+    //    WritePrivateProfileString("test123", "xValue", "6", "D:\\study_project\\Library\\MyLibrary\\ClassToIni/test.ini");
+    //}
 }
--- ClassToIni/Program.cs
+++ ClassToIni/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using com.sun.tools.doclets.formats.html;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +9,57 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-        var ini = new ClassToIni(new FileInfo(args"test.ini"));
+        //경로 지정
+        var ini = new ClassToIni(new FileInfo("test.ini"));
+        ini.CreateINI();
+        ini.LoadINI();
+        //로드한 거 확인
+        Console.WriteLine(ini.Test1);
+        Console.WriteLine(ini.Test2);
+        Console.WriteLine(ini.Test3);
+        Console.WriteLine(ini.Test4);
+        Console.WriteLine(ini.Test5);
+        Console.WriteLine(ini.Test6);
+
+        //저장할 거
+        ini.Test1 = 10;
+        ini.Test2 = "test";
+        ini.Test3 = ETest.B;
+        ini.Test4 = true;
+        ini.Test5 = 0.15434f;
+        ini.Test6 = 0.5223;
+
+        //저장
+        ini.SaveINI();
+    }
+    public enum ETest
+    {
+        A,
+        B,
+        C
+    }
+    public class Test : ClassToIni
+    {
+        //세션 네임을 지정가능 기본은 클레스 이름
+        [SectionName("Base")]
+        public int Test1 { get; set; } = 0; //기본값
+        public string Test2 { get; set; } = ""; //기본값
+        public ETest Test3 { get; set; } = ETest.A; //기본값
+        public bool Test4 { get; set; } = false; //기본값
+        public float Test5 { get; set; } = 0.0f; //기본값
+        [SectionName("Hi")]
+        public double Test6 { get; set; } = 0.0; //기본값
+
+
+
+        /// <summary>
+        /// 사용하지 않음
+        /// </summary>
+        [UseINI(false)]
+        public string NotUse { get; set; } = "";
+
+        public Test(FileInfo fileInfo) : base(fileInfo)
+        {
+        }
     }
 }
